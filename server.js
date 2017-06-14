7446ee8e04b9619161d23b30a19fa1ed0b9ad552
JPPorcel <juanpiporcel@correo.ugr.es>
2017-06-14 11:44:02 +0200

    nginx

--- server.js
+++ server.js
@@ -18,6 +18,12 @@ var connection = mysql.createConnection({
 	database : 'Music'
 });
 
+function getHashKey()
+{
+    var current_date = (new Date()).valueOf().toString();
+    var random = Math.random().toString();
+    return crypto.createHash('sha1').update(current_date + random).digest('hex');
+}
 
 
 app.use(bodyParser.urlencoded({ extended: true }));  
@@ -108,6 +114,75 @@ router.post('/users/register', function(req, res)
 			res.set({ 'content-type': 'application/json; charset=utf-8' });
 			res.send("{'message': 'user already registered'}");
 		}
+	});
+});
+
+
+router.post('/historial/nuevo', function(req, res)
+{
+    var user = req.body.user
+    var marcha = req.body.marcha
+    var claveEscucha = getHashKey()
+    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+    var date;
+    date = new Date();
+    date = date.getUTCFullYear() + '-' +
+        ('00' + (date.getUTCMonth()+1)).slice(-2) + '-' +
+        ('00' + date.getUTCDate()).slice(-2) + ' ' + 
+        ('00' + date.getUTCHours()).slice(-2) + ':' + 
+        ('00' + date.getUTCMinutes()).slice(-2) + ':' + 
+        ('00' + date.getUTCSeconds()).slice(-2);
+        
+    connection.query("select * from Usuarios where idFacebook='" + id + "'", function (err, rows)
+	{
+		if(err)
+			throw err;
+		
+		if(rows.length == 0)
+		{
+			res.set({ 'content-type': 'application/json; charset=utf-8' });
+			res.send("{'message': 'user does not exists'}");
+		}
+		else
+		{
+			connection.query("insert into Escuchas values ('"+ claveEscucha + "', '" + user + "', '" + marcha + "', " + ip + ", '" + date + "')", function (err, rows)
+            {
+                if(err)
+                    throw err;
+                
+                res.set({ 'content-type': 'application/json; charset=utf-8' });
+                res.send("{'message': 'ok'}");
+            });
+		}
+	});
+});
+
+
+router.get('/historial/:user', function(req, res)
+{
+	connection.query("select * from Escuchas where idFacebook=" + req.params.user + " order by fecha desc", function (err, rows)
+	{
+		if(err)
+			throw err;
+		
+		console.log(req.params.filtro);
+		
+		res.set({ 'content-type': 'application/json; charset=utf-8' });
+		res.send(rows);
+	});
+});
+
+router.get('/historial/:user/:n', function(req, res)
+{
+	connection.query("select * from Escuchas where idFacebook=" + req.params.user + " order by fecha desc limit "+req.params.n, function (err, rows)
+	{
+		if(err)
+			throw err;
+		
+		console.log(req.params.filtro);
+		
+		res.set({ 'content-type': 'application/json; charset=utf-8' });
+		res.send(rows);
 	});
 });
 
